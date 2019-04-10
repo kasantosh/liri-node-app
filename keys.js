3cad1ec866fc0b82f5a296aee6a2821d2923b447
Santosh Kumar <kasantosh@yahoo.com>
2019-04-10 19:24:09 -0400

    All links work except for spotify

--- keys.js
+++ keys.js
@@ -1,6 +1,4 @@
-// console.log("this is loaded");
-
-// exports.spotify = {
-//   id:d2f5f6c2c11141798893cf709e1d63f3,
-//   secret:42fde0aaadbb4639bbda9e970b7dd25
-// };
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,27 +1,118 @@
 require("dotenv").config();
 
 var Spotify = require("node-spotify-api");
-var Axios = require("axios");
+var axios = require("axios");
 var keys = require("./keys.js");
+var moment = require("moment");
+var fs = require("fs");
 
-// var spotify = new Spotify({
-//     id: d2f5f6c2c11141798893cf709e1d63f3,
-//     secret: 42fde0aaadbb4639bbda9e970b7dd25d
-//   });
+var action = process.argv[2];
+var query = process.argv.slice(3).join(" ");
+//console.log(argument, query);
 
-// spotify.search({ type: "track", query: "All the Small Things" }, function(
-//   err,
-//   data
-// ) {
-//   if (err) {
-//     return console.log("Error occurred: " + err);
-//   }
+switch (action) {
+  case "concert-this":
+    //bands in town
+    concertThis(query);
+    break;
 
-//   console.log(data);
-// });
+  case "spotify-this-song":
+    //go to spotify
+    var spotify = new Spotify(keys.spotify);
+    console.log(keys.spotify);
+    spotify
+      .search({ type: "track", query: "All the Small Things" })
+      .then(function(response) {
+        console.log(response.href);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    break;
+  case "movie-this":
+    //go to axios OMDB
+    movieThis(query);
 
-var argvlength = process.argv.length;
-var argument = process.argv[2];
-var query = process.argv[3];
+    break;
+  case "do-what-it-says":
+    //go to spotify
+    fs.readFile("random.txt", "utf-8", function(err, data) {
+      if (err) {
+        return console.log("Error");
+      }
+      //do-what-it-says
+      var queryArray = data.split(",");
+      // console.log(data);
+      // console.log(queryArray);
+      var dothisAction = queryArray[0];
+      var dothisQuery = queryArray[1];
+      // console.log(dothisAction, dothisQuery);
+      query = dothisQuery.split(" ");
+      query = query[0];
+      // console.log(query);
+      concertThis(query);
 
-console.log(argvlength);
+      // var  = q.split(" ");
+      // console.log(b);
+
+      // switch (a) {
+      //   case "movie-this":
+      //     movieThis();
+      //     break;
+      //   case "concert-this":
+      //     concertThis(b[0]);
+      // }
+    });
+    break;
+}
+
+function movieThis(query1) {
+  console.log(query);
+  axios
+    .get(
+      "http://www.omdbapi.com/?t=" + query1 + "&y=&plot=short&apikey=trilogy"
+    )
+    .then(function(response) {
+      console.log("=====================================");
+      console.log("The movie title is " + response.data.Title);
+      console.log("Year of release: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+      console.log("Country Produced: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+      console.log("=====================================");
+      // console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function concertThis(query1) {
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        query1 +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      console.log("=====================================");
+      for (var e in response.data) {
+        console.log(response.data[e].venue.name);
+        console.log(
+          response.data[e].venue.city + ", " + response.data[e].venue.country
+        );
+        var date = moment(response.data[e].datetime).format("MM/DD/YYYY");
+        var time = moment(response.data[e].datetime).format("HH:mm");
+        console.log(date);
+        console.log(time);
+        // console.log(response.data);
+        console.log("=====================================");
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
